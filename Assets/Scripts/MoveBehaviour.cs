d593f3680f6c2a3c8f54daad19c02e26a2dcf635
Maynlie <clement.blaise@gmail.com>
2018-10-27 00:40:57 +0200

    Move Right and Left

---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBehaviour : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float vitesse;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("left"))
+        {
+            rb.AddForce(-transform.right * vitesse);
+        }
+        else if(Input.GetKey("right"))
+        {
+            rb.AddForce(transform.right * vitesse);
+        }
+        else if(Input.GetKeyUp("left"))
+        {
+            rb.AddForce(transform.right * vitesse);
+        }
+        else if (Input.GetKeyUp("right"))
+        {
+            rb.AddForce(-transform.right * vitesse);
+        }
+    }
+}
